a0a6ac73297896f8eff4e84aa47914ad7833db91
yeon <rladusdn56@gmail.com>
2024-07-12 15:50:46 +0900

    가위바위보 간단버전

--- codeit-dicegame/src/index.js
+++ codeit-dicegame/src/index.js
@@ -1,4 +1,12 @@
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+root.render(
+    <>
+        <h1 id="title">가위바위보</h1>
+        <button className="hand">가위</button>
+        <button className="hand">바위</button>
+        <button className="hand">보</button>
+
+    </>
+);

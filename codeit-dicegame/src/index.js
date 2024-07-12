de20faad008b8ba850c9b416a1002920347071cd
yeon <rladusdn56@gmail.com>
2024-07-12 16:10:54 +0900

    가위바위보 수정2

--- codeit-dicegame/src/index.js
+++ codeit-dicegame/src/index.js
@@ -1,12 +1,34 @@
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <>
-        <h1 id="title">가위바위보</h1>
-        <button className="hand">가위</button>
-        <button className="hand">바위</button>
-        <button className="hand">보</button>
 
-    </>
+const WINS = {
+    rock: 'scissor',
+    scissor: 'paper',
+    paper: 'rock',
+  };
+
+function getResult(left, right) {
+    if (WINS[left] === right) return '승리';
+    else if (left === WINS[right]) return '패배';
+    return '무승부';
+  }
+
+function handleClick() {
+    console.log('가위바위보!');
+  }
+  
+const me = 'rock';
+const other = 'scissor';
+
+const result = getResult(me, other);
+  
+root.render(
+  <>
+    <h1>가위바위보</h1>
+    <h2>{result}</h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+  </>
 );

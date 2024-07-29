0f62e6e801cfa54123a0be372954915147fa24c9
yeon <rladusdn56@gmail.com>
2024-07-29 18:58:25 +0900

    Codeit 3주차 실습 fin

--- codeit-rsp/src/HandButton.js
+++ codeit-rsp/src/HandButton.js
@@ -1,13 +1,13 @@
-import HandIcon from "./HandIcon";
+import HandIcon from './HandIcon';
+import './HandButton.css';
 
 function HandButton({ value, onClick }) {
-    const handleClick = () => onClick(value);
-    return (
-    <button onClick={handleClick}>
-        <HandIcon value={value}/>
+  const handleClick = () => onClick(value);
+  return (
+    <button className="HandButton" onClick={handleClick}>
+      <HandIcon className="HandButton-icon" value={value} />
     </button>
-        );
-  }
-  
-  export default HandButton;
-  
+  );
+}
+
+export default HandButton;
